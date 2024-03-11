60c730bc06c87c9e640b0008dd79b46504e013a1
mhoopmann <michael.hoopmann@systemsbiology.org>
2024-03-11 16:26:43 -0700

    update the version number

--- MagnumManager.h
+++ MagnumManager.h
@@ -4,8 +4,8 @@
 #include "MLog.h"
 #include "MParams.h"
 
-#define VERSION "1.3.1"
-#define BDATE "August 30 2023"
+#define VERSION "1.3.2"
+#define BDATE "March 11 2024"
 
 class MagnumManager {
 public:
